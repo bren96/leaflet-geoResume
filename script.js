ab5701342973d06f5dbeff0fe12ca5a47646fe92
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2020-05-19 07:49:34 -0400

    add leaflet and mapbox

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function job_click(){
-  document.getElementById('map').scrollIntoView();
+  document.getElementById('mapid').scrollIntoView();
 };
+
+var resume_map = L.map('mapid').setView([51.505, -0.09], 13);
+
+var mapbox_tolken = 'pk.eyJ1IjoiYnJlbjk2IiwiYSI6ImNqc2pkNGRvdTA0bm80OW9hOTIxNzB6NG0ifQ.tDovHyl1gFWQ96O3pok0Qg';
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: mapbox_tolken
+}).addTo(resume_map);
