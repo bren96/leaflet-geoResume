f7cfd526736d7fa540c216572bd82e4216bed8c1
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2020-05-23 19:08:09 -0400

    add resume data

---
+++ resumeData.js
@@ -0,0 +1,85 @@
+var resumeData = [{
+    "type": "Feature",
+    "id": "01",
+    "properties": {
+      "position": "Planning Technician",
+      "employer": "Chautauqua County Division of Planning & Community Development",
+      "period": "Jan 20' - Present",
+      "responsibilities": "Develop static maps, web-maps, and other cartographic products. Analyze and organize data for planning studies. Assist in the preparation and administration of county planning programs and grants."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-79.578113, 42.321769]
+    }
+  },
+  {
+    "type": "Feature",
+    "id": "02",
+    "properties": {
+      "position": "GIS Programmer/Analyst",
+      "employer": "Seneca County Auditor's Office",
+      "period": "Jan 19' - Jan 20'",
+      "responsibilities": "Maintain, further develop, and promote the Geographic Information Data Library of Seneca County. Automate data processing workflows between the Auditor’s Office, other departments, and outside contractors. Produce agricultural use valuation reports."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-83.176393, 41.114497]
+    }
+  },
+  {
+    "type": "Feature",
+    "id": "03",
+    "properties": {
+      "position": "Planning & GIS Intern",
+      "employer": "Orange County Department of Planning",
+      "period": "Jun 18’ – Aug 18’'",
+      "responsibilities": "Produced cartographic and spatial data products using ArcGIS for county planning projects and various advocacy and development groups. Built suitability model tools using Modelbuilder and Python. Created diagrams and figures for county reports."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-74.321344, 41.402518]
+    }
+  },
+  {
+    "type": "Feature",
+    "id": "04",
+    "properties": {
+      "position": "Student Researcher",
+      "employer": "Geneseo Research Assistantship",
+      "period": "Jan 18’– May 18’",
+      "responsibilities": "Funded by the Dean Johnston Foundation Award. Assisted supervising professor with production and organization of forest-modeling results. Developed various maps and figures using ArcGIS and Excel. Contributed to publication of research material."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-77.818140, 42.796371]
+    }
+  },
+  {
+    "type": "Feature",
+    "id": "05",
+    "properties": {
+      "position": "Sustainability Intern",
+      "employer": "Geneseo Office of Sustainability",
+      "period": "Aug 16’ – May 18’",
+      "responsibilities": "Managed general office communication. Organized sustainability-focused community events and workshops. Coordinated Tree Campus USA Recertification. Managed SUNY Geneseo eGarden projects."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-77.819914, 42.796859]
+    }
+  },
+  {
+    "type": "Feature",
+    "id": "06",
+    "properties": {
+      "position": "NSF Research Assistant",
+      "employer": "SUNY Research Foundation",
+      "period": "May 17’– Aug 17’",
+      "responsibilities": "Transcribed qualitative historical information into a GIS database. Geo-referenced and digitized historical maps using ArcGIS. Contributed to the long-term research goals."
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-77.818560, 42.796330]
+    }
+  }
+];
