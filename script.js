776150f0523fc91466cd5547300c6334e5d4d1c7
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2020-05-26 18:05:49 -0400

    add mapbox custom style

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function job_click(){
 
 var resume_map = L.map('mapid').setView([0,0],8);
 
-var mapbox_tolken = 'pk.eyJ1IjoiYnJlbjk2IiwiYSI6ImNqc2pkNGRvdTA0bm80OW9hOTIxNzB6NG0ifQ.tDovHyl1gFWQ96O3pok0Qg';
+var mapbox_token = 'pk.eyJ1IjoiYnJlbjk2IiwiYSI6ImNqc2pkNGRvdTA0bm80OW9hOTIxNzB6NG0ifQ.tDovHyl1gFWQ96O3pok0Qg';
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+
+L.tileLayer('https://api.mapbox.com/styles/v1/bren96/ckaofh5j60z7f1iqpba652b1r/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYnJlbjk2IiwiYSI6ImNqc2pkNGRvdTA0bm80OW9hOTIxNzB6NG0ifQ.tDovHyl1gFWQ96O3pok0Qg',
+  {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: mapbox_tolken
 }).addTo(resume_map);
 
 // create scroll button control
