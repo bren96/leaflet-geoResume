09912dc153c6010ee4b62212c4895526a07184b8
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2020-05-27 12:29:57 -0400

    match resume background to map background

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ var resume_map = L.map('mapid').setView([0,0],8);
 var mapbox_token = 'pk.eyJ1IjoiYnJlbjk2IiwiYSI6ImNqc2pkNGRvdTA0bm80OW9hOTIxNzB6NG0ifQ.tDovHyl1gFWQ96O3pok0Qg';
 var mapbox_style = 'https://api.mapbox.com/styles/v1/bren96/{id}/tiles/256/{z}/{x}/{y}@2x?access_token={access_token}'
 
-
 L.tileLayer(mapbox_style, {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
