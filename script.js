b322c831d601780fb38bf3312876ae867400776f
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2020-05-27 18:35:13 -0400

    fix flyTo by moving openPopup after

--- script.js
+++ script.js
@@ -48,15 +48,16 @@ resume_map.fitBounds(points.getBounds());
 // on click a button in the resume section
 function job_click(a){
   var b = points.getLayers();
-  var lat = b[a].feature.geometry.coordinates[1] + 0.007;
+  var lat = b[a].feature.geometry.coordinates[1] + 0.001;
   var long = b[a].feature.geometry.coordinates[0];
   var lat_long = [lat,long];
-  b[a].openPopup();
-  resume_map.flyTo(lat_long, 14, {
+  resume_map.flyTo(lat_long, 13, {
     animate:true,
     easeLinearity: 1.0,
-    duration: 2
+    duration: 3,
+    noMoveStart: true,
   });
+  b[a].openPopup();
   // resume_map.setView(lat_long,14);
   document.getElementById("mapid").scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
 };
